292d10e59115a487f1be7933cb6b9c45a710cc5a
Nazmul Haque Naqib <39087050+nokibsarkar@users.noreply.github.com>
2023-07-17 00:29:44 +0600

    limit.limit is not an integer, rather a pointer to integer. Therefore, it needs the conversion and it is working now.
    I downloaded your package using `go get  github.com/zhaobingss/sqlite` but it seems like you changed your code since.

--- sqlite.go
+++ sqlite.go
@@ -1,11 +1,11 @@
-package sqlcipher
+package sqlite
 
 import (
 	"database/sql"
 	"strconv"
 	"strings"
 
-	_ "github.com/zhaobingss/go-sqlcipher/v4"
+	_ "github.com/mutecomm/go-sqlcipher/v4"
 	"gorm.io/gorm"
 	"gorm.io/gorm/callbacks"
 	"gorm.io/gorm/clause"
@@ -81,12 +81,12 @@ func (dialector Dialector) ClauseBuilders() map[string]clause.ClauseBuilder {
 		},
 		"LIMIT": func(c clause.Clause, builder clause.Builder) {
 			if limit, ok := c.Expression.(clause.Limit); ok {
-				if limit.Limit > 0 {
+				if *limit.Limit > 0 {
 					builder.WriteString("LIMIT ")
-					builder.WriteString(strconv.Itoa(limit.Limit))
+					builder.WriteString(strconv.Itoa(*limit.Limit))
 				}
 				if limit.Offset > 0 {
-					if limit.Limit > 0 {
+					if *limit.Limit > 0 {
 						builder.WriteString(" ")
 					}
 					builder.WriteString("OFFSET ")
